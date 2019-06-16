a9f19bbaa19c48499425c286f1300b452a0ce787
emile <51802782+emilepennes@users.noreply.github.com>
2019-06-16 20:47:43 +0200

    Update Polynome_second_degre_resolution.py

--- Polynome_second_degre_resolution.py
+++ Polynome_second_degre_resolution.py
@@ -1,6 +1,6 @@
 
 # coding:utf-8
-#Le but de l'exercice est de définir un programme qui pour toute équation aX^2 + bX + c = 0, a,b,c étant réels, nous donne les solutions réelles.
+# Le but de l'exercice est de définir un programme qui pour toute équation aX^2 + bX + c = 0, a,b,c étant réels, nous donne les solutions réelles.
 
 from math import *
 
