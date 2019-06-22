2acbc5f0a9dc20d7f5b735e37fbf4ba1b74aea64
emile <51802782+emilepennes@users.noreply.github.com>
2019-06-22 19:46:28 +0200

    Update Polynome_second_degre_resolution.py

--- Polynome_second_degre_resolution.py
+++ Polynome_second_degre_resolution.py
@@ -1,52 +1,58 @@
 # coding:utf-8
-# Le but de l'exercice est de définir un programme qui pour toute équation aX^2 + bX + c = 0, a,b,c étant réels, nous donne les solutions réelles.
+""" Le but de l'exercice est de définir un programme qui pour toute équation
+aX^2 + bX + c = 0, a,b,c étant réels, nous donne les solutions réelles."""
 
-from math import *
+from math import sqrt
 
-print("Le but de cet exercice est de définir un programme qui pour toute équation aX^2 + bX + c = 0 nous donne les solutions réelles.")
+print("Le but de cet exercice est de définir un programme qui",
+      " pour toute équation aX^2 + bX + c = 0 nous donne les solutions réelles.")
 
 def input_value(x):
-    while True :
-        try :
+    """Input float value"""
+    while True:
+        try:
             x = float(input("Soit "+ x +" = "))
             return x
-        except ValueError :
+        except ValueError:
             print(x, "doit être un nombre réel ;)  Try again...")
 
-a = "a"
-b = "b"
-c = "c"
-a = input_value(a)
-b = input_value(b)
-c = input_value(c)
+A = "a"
+B = "b"
+C = "c"
+A = input_value(A)
+B = input_value(B)
+C = input_value(C)
 
 
-if a!=0 :
-    print("Soit l\'équation: ", a, "*X^2 +", b, "*X +", c, "=0")
-    delta = b**2-4*a*c
-    print("le discriminant a pour valeur", delta,".")
-        
-    if delta < 0 :
-	    print("Il n y a pas de racine réelle à cette équation.")
+if A != 0:
+    print("Soit l\'équation: ", A, "*X^2 +", B, "*X +", C, "=0")
+    DELTA = B**2-4*A*C
+    print("le discriminant a pour valeur", DELTA, ".")
 
-    elif delta == 0:
-	    X1 = -b/(2*a)
-	    print("la solution de cette équation est une racine double nommée X1 qui a pour valeur ", X1,".") 
+    if DELTA < 0:
+        print("Il n y a pas de racine réelle à cette équation.")
+
+    elif DELTA == 0:
+        X1 = -B/(2*A)
+        print("la solution de cette équation",
+	      " est une racine double nommée X1 qui a pour valeur ", X1, ".")
 
     else:
-	    X1 = -b - sqrt(delta)/(2*a)
-	    X2 = -b + sqrt(delta)/(2*a)
-	    print("il y a deux racines distinctes à cette équation nommées X1 et X2 qui ont pour valeurs respectives ", X1, "et ", X2,".")
+        X1 = -B - sqrt(DELTA)/(2*A)
+        X2 = -B + sqrt(DELTA)/(2*A)
+        print("il y a deux racines distinctes à cette équation",
+              " nommées X1 et X2 qui ont pour valeurs respectives ", X1, "et ", X2, ".")
 
-else :
-    if b != 0 :
-        print( "Soit l\'équation:", b, "*X +", c, "=0")
-        X1 = -c/b
-        print("la solution de cette équation est une racine simple nommée X1 qui a pour valeur ", X1,".")
-    else :
-        if c != 0 :
-            print("Soit l\'équation:", c, "=0")
+else:
+    if B != 0:
+        print("Soit l\'équation:", B, "*X +", C, "=0")
+        X1 = -C/B
+        print("la solution de cette équation est",
+              " une racine simple nommée X1 qui a pour valeur ", X1, ".")
+    else:
+        if C != 0:
+            print("Soit l\'équation:", C, "=0")
             print("cette équation n\'a pas de solution.")
-        else : 
+        else:
             print("Soit l\'équation: 0=0")
             print("cela fait la tête à toto !;)")
